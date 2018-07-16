bf872deffd8956c92a2158831bd57e9bfa403613
Florian PETIT <florianp37@me.com>
2018-07-16 14:28:18 +0200

    add swift package manager (SPM) support

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "xcbetarunner",
+    dependencies: [],
+    targets: [
+        .target(
+            name: "xcbetarunner",
+            dependencies: [],
+            path: "xcbetarunner"),
+    ]
+)
